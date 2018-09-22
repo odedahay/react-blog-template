137d2b73d0a887f7e7a2dbfcb9209c557736f5d5
Rodelio <odedahay@gmail.com>
2018-09-22 15:28:20 +0800

    finish the UI

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import Welcome from './components/Welcome';
+import SingleArticle from './components/SingleArticle';
+import CreateArticle from './components/CreateArticle';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+
+
+ReactDOM.render(
+    <BrowserRouter>
+        <div>
+            <NavBar/>
+            <Route exact path="/" component={Welcome} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={SignUp} />
+            <Route path="/article/:slug" component={SingleArticle} />
+            <Route path="/article/create" component={CreateArticle} />
+            <Footer />
+
+        </div>
+
+
+    </BrowserRouter>
+    ,document.getElementById('root'));
+    registerServiceWorker();
